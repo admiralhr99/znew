30deb2b8f11f4b872b2f9b1bb78d96a1f8055bbe
admiralhr99 <admiralhr99@gmail.com>
2024-07-28 08:10:39 -0700

    improve it with channel and goroutine

--- main.go
+++ main.go
@@ -4,169 +4,93 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
-	"log"
+	"io"
 	"os"
+	"strings"
 	"sync"
 )
 
-func appendLineToFile(filename string, linesChan <-chan string, wg *sync.WaitGroup, linesFile *map[string]struct{}) {
-	defer wg.Done()
-
-	f, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
-	if err != nil {
-
-		f, err = os.Create(filename)
-		if err != nil {
-			log.Fatalf("failed creating file: %s", err)
-
-		}
-	}
-
-	defer func() {
-		err := f.Close()
-		if err != nil {
-			log.Fatalf("failed to close file: %s", err)
-		}
-	}()
-	fileInfo, err := f.Stat()
-	if err != nil {
-		log.Fatalf("failed to get file info: %s", err)
-	}
-
-	lastByte := make([]byte, 1)
-	if fileInfo.Size() > 0 {
-		offset, err := f.Seek(-1, 2)
-		if err != nil {
-			fmt.Println("Error seeking file:", err)
-			return
-		}
-
-		_, err = f.ReadAt(lastByte, offset)
-		if err != nil {
-			fmt.Println("Error reading file:", err)
-			return
-		}
-		for line := range linesChan {
-
-			if line == "" {
-				continue
-			} else if lastByte[0] == '\n' {
-				_, err = fmt.Fprintln(f, line)
-
-			} else {
-				_, err = fmt.Fprintf(f, "\n%s", line)
-
-			}
-			if err != nil {
-				log.Fatal("failed writing to file: ", err)
-			}
-			(*linesFile)[line] = struct{}{}
-		}
-
-	} else {
-
-		for line := range linesChan {
-
-			if line == "" {
-				continue
-			}
-			if lastByte[0] != '\n' {
-				_, err = fmt.Fprintln(f, line)
-
-			} else {
-				_, err = fmt.Fprintf(f, "\n%s", line)
-
-			}
-			if err != nil {
-				log.Fatal("failed writing to file: ", err)
-			}
-			(*linesFile)[line] = struct{}{}
-		}
-	}
-}
-
-func readLinesFromStdin(linesChan chan<- string, wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		line := scanner.Text()
-		if line != "" {
-			linesChan <- scanner.Text()
-		}
-	}
-	close(linesChan)
-}
-
-func readLinesFromFile(filename string, linesFile *map[string]struct{}) {
-	file, err := os.Open(filename)
-	if err != nil {
-		file, err = os.Create(filename)
-		if err != nil {
-			log.Fatalf("failed creating file: %s", err)
-
-		}
-	}
-	defer func(file *os.File) {
-		err := file.Close()
-		if err != nil {
-			log.Fatalf("failed to close file: %s\n", err)
-		}
-	}(file)
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
-
-		if line != "" {
-
-			(*linesFile)[line] = struct{}{}
-		} else {
-			fmt.Println("Your file contains empty lines that might be causing issues. Please remove them and try again.")
-			fmt.Println("You can do this with the following command:")
-			fmt.Println("sed -i '/^$/d'", filename)
-			break
-		}
-	}
-
-}
-
 func main() {
-	var quietMode bool
-	var dryRun bool
-	var trim bool
+	var quietMode, dryRun, trim bool
 	flag.BoolVar(&quietMode, "q", false, "quiet mode (no output at all)")
 	flag.BoolVar(&dryRun, "d", false, "don't append anything to the file, just print the new lines to stdout")
 	flag.BoolVar(&trim, "t", false, "trim leading and trailing whitespace before comparison")
-	fileTwo := flag.String("f", "", "file to compare with stdin")
 	flag.Parse()
 
-	linesFile := make(map[string]struct{})
-	readLinesFromFile(*fileTwo, &linesFile)
+	fn := flag.Arg(0)
 
-	linesChanRead := make(chan string)  // read from stdin
-	linesChanWrite := make(chan string) // write to file
+	lines := readExistingLines(fn, trim)
+
+	var f io.WriteCloser
+	var err error
+	if fn != "" && !dryRun {
+		f, err = os.OpenFile(fn, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to open file for writing: %s\n", err)
+			return
+		}
+		defer f.Close()
+	}
+
+	processInput(lines, f, quietMode, dryRun, trim)
+}
+
+func readExistingLines(fn string, trim bool) sync.Map {
+	var lines sync.Map
+	if fn == "" {
+		return lines
+	}
+
+	r, err := os.Open(fn)
+	if err != nil {
+		return lines
+	}
+	defer r.Close()
+
+	sc := bufio.NewScanner(r)
+	for sc.Scan() {
+		line := sc.Text()
+		if trim {
+			line = strings.TrimSpace(line)
+		}
+		lines.Store(line, true)
+	}
+	return lines
+}
+
+func processInput(lines sync.Map, f io.Writer, quietMode, dryRun, trim bool) {
+	sc := bufio.NewScanner(os.Stdin)
 	var wg sync.WaitGroup
-	wg.Add(2)
-	go readLinesFromStdin(linesChanRead, &wg)
-	go appendLineToFile(*fileTwo, linesChanWrite, &wg, &linesFile)
+	inputChan := make(chan string)
 
-	for line := range linesChanRead {
-		if linesChanRead != nil {
-			if _, exists := linesFile[line]; !exists {
-				if quietMode == true {
-					linesChanWrite <- line
-				} else if dryRun == true {
-					fmt.Println(line)
-				} else {
-					fmt.Println(line)
-					linesChanWrite <- line
-				}
+	// Start worker goroutines
+	for i := 0; i < 4; i++ {
+		wg.Add(1)
+		go processLine(inputChan, &lines, f, quietMode, dryRun, trim, &wg)
+	}
+
+	// Read input and send to workers
+	for sc.Scan() {
+		inputChan <- sc.Text()
+	}
+	close(inputChan)
+
+	wg.Wait()
+}
+
+func processLine(inputChan <-chan string, lines *sync.Map, f io.Writer, quietMode, dryRun, trim bool, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for line := range inputChan {
+		if trim {
+			line = strings.TrimSpace(line)
+		}
+		if _, exists := lines.LoadOrStore(line, true); !exists {
+			if !quietMode {
+				fmt.Println(line)
+			}
+			if !dryRun && f != nil {
+				fmt.Fprintf(f, "%s\n", line)
 			}
 		}
 	}
-
-	close(linesChanWrite)
-	wg.Wait()
-
 }
